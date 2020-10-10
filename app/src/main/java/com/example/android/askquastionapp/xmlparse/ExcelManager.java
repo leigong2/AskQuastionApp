1084d9dd7e0328eff287873618b55b70b8ad0536
wangzhilong <3408743418@qq.com>
2020-10-10 11:12:00 +0800

    xlsx解析

--- app/src/main/java/com/example/android/askquastionapp/xmlparse/ExcelManager.java
+++ app/src/main/java/com/example/android/askquastionapp/xmlparse/ExcelManager.java
@@ -1,23 +1,25 @@
 package com.example.android.askquastionapp.xmlparse;
 
 import android.util.Log;
-import android.util.Xml;
 
-import org.xmlpull.v1.XmlPullParser;
+import org.apache.poi.hssf.usermodel.HSSFDateUtil;
+import org.apache.poi.ss.usermodel.CellValue;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
-import java.util.zip.ZipInputStream;
 
 import jxl.Cell;
 import jxl.Sheet;
@@ -50,125 +52,82 @@ public class ExcelManager {
         return instance;
     }
 
-    public Map<String, List<List<String>>> analyzeXlsx(String fileName) {
-        Map<String, List<List<String>>> map = new HashMap<>();
-        InputStream isShareStrings = null;
-        InputStream isXlsx = null;
-        ZipInputStream zipInputStream = null;
-        listCells = new ArrayList<>();
+    public Map<String, List<List<String>>> analyzeXlsx(File fileName) {
+        Map<String, List<List<String>>> data = new HashMap<>();
         try {
-            ZipFile zipFile = new ZipFile(new File(fileName));
-            ZipEntry sharedStringXML = zipFile.getEntry(SHAREDSTRINGS);
-            isShareStrings = zipFile.getInputStream(sharedStringXML);
-            //
-            XmlPullParser xmlPullParser = Xml.newPullParser();
-            xmlPullParser.setInput(isShareStrings, "utf-8");
-            int eventType = xmlPullParser.getEventType();
-            while (eventType != xmlPullParser.END_DOCUMENT) {
-                switch (eventType) {
-                    case XmlPullParser.START_TAG:
-                        String tag = xmlPullParser.getName();
-                        if ("t".equals(tag)) {
-                            listCells.add(xmlPullParser.nextText());
-                        }
-                        break;
-                    default:
-                        break;
-                }
-                eventType = xmlPullParser.next();
-            }
-            Log.i("zzz", "analyze: list --> " + listCells);
-            //
-            isXlsx = new BufferedInputStream(new FileInputStream(fileName));
-            zipInputStream = new ZipInputStream(isXlsx);
-            ZipEntry zipDir;
-            while ((zipDir = zipInputStream.getNextEntry()) != null) {
-                String dirName = zipDir.getName();
-                if (!zipDir.isDirectory() && dirName.endsWith(ENDXML)) {
-                    if (dirName.contains(DIRSHEET)) {
-                        parseSheet(zipFile, dirName, map);
+            InputStream stream = new FileInputStream(fileName);
+            XSSFWorkbook workbook = new XSSFWorkbook(stream);
+            for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
+                XSSFSheet sheet = workbook.getSheetAt(0);
+                int rowsCount = sheet.getPhysicalNumberOfRows();
+                FormulaEvaluator formulaEvaluator = workbook.getCreationHelper().createFormulaEvaluator();
+                List<List<String>> list = new ArrayList<>();
+                for (int r = 0; r < rowsCount; r++) {
+                    Row row = sheet.getRow(r);
+                    int cellsCount = row.getPhysicalNumberOfCells();
+                    List<String> list3 = new ArrayList<>();
+                    for (int c = 0; c < cellsCount; c++) {
+                        String cellAsString = getCellAsString(row, c, formulaEvaluator);
+                        list3.add(cellAsString);
                     }
+                    list.add(list3);
                 }
+                data.put(workbook.getSheetName(i), list);
             }
         } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                zipInputStream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                isXlsx.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                isShareStrings.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            /* proper exception handling to be here */
         }
-        Iterator<Map.Entry<String, List<List<String>>>> iterator = map.entrySet().iterator();
-        while (iterator.hasNext()) {
-            Map.Entry<String, List<List<String>>> next = iterator.next();
-            Iterator<List<String>> iterator1 = next.getValue().iterator();
-            while (iterator1.hasNext()) {
-                Log.i("zzz", "analyzeXls: sheet --> " + next.getKey() + " row --> " + iterator1.next());
-            }
-        }
-        return map;
+        return data;
     }
 
-    private void parseSheet(ZipFile zipFile, String entryName, Map<String, List<List<String>>> map) {
-        int lastIndexOf = entryName.lastIndexOf(File.separator);
-        String sheetName = entryName.substring(lastIndexOf + 1, entryName.length() - 4);
-        //
-        String v = null;
-        List<String> colums = null;
-        List<List<String>> rows = new ArrayList<>();
-        InputStream inputStreamSheet = null;
-        try {
-            ZipEntry sheet = zipFile.getEntry(entryName);
-            inputStreamSheet = zipFile.getInputStream(sheet);
-            XmlPullParser xmlPullParserSheet = Xml.newPullParser();
-            xmlPullParserSheet.setInput(inputStreamSheet, "utf-8");
-            int evenTypeSheet = xmlPullParserSheet.getEventType();
-            while (xmlPullParserSheet.END_DOCUMENT != evenTypeSheet) {
-                switch (evenTypeSheet) {
-                    case XmlPullParser.START_TAG:
-                        String tag = xmlPullParserSheet.getName();
-                        if ("row".equalsIgnoreCase(tag)) {
-                            colums = new ArrayList<>();
-                        } else if ("v".equalsIgnoreCase(tag)) {
-                            v = xmlPullParserSheet.nextText();
-                            if (v != null) {
-                                colums.add(listCells.get(Integer.parseInt(v)));
-                            } else {
-                                colums.add(v);
-                            }
-                        }
-                        break;
-                    case XmlPullParser.END_TAG:
-                        if ("row".equalsIgnoreCase(xmlPullParserSheet.getName()) && v != null) {
-                            rows.add(colums);
-                        }
-                        break;
-                }
-                evenTypeSheet = xmlPullParserSheet.next();
+    //Map转Object
+    public <T> T mapToObject(Map<Object, Object> map, Class<T> beanClass) throws Exception {
+        if (map == null)
+            return null;
+        T obj = beanClass.newInstance();
+        Field[] fields = obj.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            int mod = field.getModifiers();
+            if (Modifier.isStatic(mod) || Modifier.isFinal(mod)) {
+                continue;
             }
-            if (rows != null && rows.size() > 0) {
-                map.put(sheetName, rows);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                inputStreamSheet.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+            field.setAccessible(true);
+            if (map.containsKey(field.getName())) {
+                field.set(obj, map.get(field.getName()));
             }
         }
+        return obj;
+    }
+
+    protected String getCellAsString(Row row, int c, FormulaEvaluator formulaEvaluator) {
+        String value = "";
+        try {
+            org.apache.poi.ss.usermodel.Cell cell = row.getCell(c);
+            CellValue cellValue = formulaEvaluator.evaluate(cell);
+            switch (cellValue.getCellType()) {
+                case org.apache.poi.ss.usermodel.Cell.CELL_TYPE_BOOLEAN:
+                    value = "" + cellValue.getBooleanValue();
+                    break;
+                case org.apache.poi.ss.usermodel.Cell.CELL_TYPE_NUMERIC:
+                    double numericValue = cellValue.getNumberValue();
+                    if (HSSFDateUtil.isCellDateFormatted(cell)) {
+                        double date = cellValue.getNumberValue();
+                        SimpleDateFormat formatter =
+                                new SimpleDateFormat("dd/MM/yy");
+                        value = formatter.format(HSSFDateUtil.getJavaDate(date));
+                    } else {
+                        value = "" + numericValue;
+                    }
+                    break;
+                case org.apache.poi.ss.usermodel.Cell.CELL_TYPE_STRING:
+                    value = "" + cellValue.getStringValue();
+                    break;
+                default:
+            }
+        } catch (NullPointerException e) {
+            /* proper error handling should be here */
+        }
+        return value;
     }
 
     public Map<String, List<List<String>>> analyzeXls(String fileName) {
